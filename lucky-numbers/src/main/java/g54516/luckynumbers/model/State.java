5cbbf4baaaafd39e6818f95caa638528416bf657
54516 <54516@etu.he2b.be>
2021-03-15 14:32:56 +0100

    Ajout de l'énumération State (section 4.1)

---
+++ lucky-numbers/src/main/java/g54516/luckynumbers/model/State.java
@@ -0,0 +1,30 @@
+package g54516.luckynumbers.model;
+
+/**
+ * Represents the state of the game.
+ *
+ * @author Oumar Magomadov <54516@etu.he2b.be>
+ */
+public enum State {
+    /**
+     * Represents that the game is not started.
+     */
+    NOT_STARTED,
+    /**
+     * Represents that the player must pick a tile.
+     */
+    PICK_TILE,
+    /**
+     * Represents that the player must place a tile.
+     */
+    PLACE_TILE,
+    /**
+     * Represents that the player finished his action and next player is
+     * selected.
+     */
+    TURN_END,
+    /**
+     * Represents that a player has completed his board.
+     */
+    GAME_OVER;
+}
