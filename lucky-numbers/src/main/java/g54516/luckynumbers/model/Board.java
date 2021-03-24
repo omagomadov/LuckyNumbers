b067bb44840aba03443876c8e6d4cf9804e9f747
Oumar <Oumar@192.168.0.172>
2021-03-24 09:17:01 +0100

    Modifications de la class Board (section 3.3) + Correction du 'if' de la méthode canTileBePut()

--- lucky-numbers/src/main/java/g54516/luckynumbers/model/Board.java
+++ lucky-numbers/src/main/java/g54516/luckynumbers/model/Board.java
@@ -92,15 +92,12 @@ public class Board {
      * higher than those on the left and up on the board
      */
     private boolean tileCanBePut(Tile tile, Position pos) {
-        if (isTileLowerDown(tile, pos) && isTileLowerRight(tile, pos)) {
-            if (!isTileHigherUp(tile, pos) || !isTileHigherLeft(tile, pos)) {
-                return false;
-            }
+        if (isTileLowerDown(tile, pos) && isTileLowerRight(tile, pos)
+                && isTileHigherUp(tile, pos) && isTileHigherLeft(tile, pos)) {
+            return true;
         } else {
             return false;
         }
-
-        return true;
     }
 
     /**
