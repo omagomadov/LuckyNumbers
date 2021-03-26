a5bdb9fb7aa568b4ec69d0034c663e7f3c70ab10
Oumar <Oumar@192.168.0.173>
2021-03-26 15:43:54 +0100

    Modifications de la class GameTest (section 4.3 et 4.4) + Ajouts de tests supplémentaires

--- lucky-numbers/src/test/java/g54516/luckynumbers/model/GameTest.java
+++ lucky-numbers/src/test/java/g54516/luckynumbers/model/GameTest.java
@@ -106,8 +106,7 @@ public class GameTest {
     }
 
     /* === À vous de compléter... === */
-    
-    /* === Testing method canTileBePut() === */
+ /* === Testing method canTileBePut() === */
     @Test
     public void can_tile_be_put_false() {
         game.start(2);
@@ -126,6 +125,13 @@ public class GameTest {
         assertTrue(game.canTileBePut(new Position(0, 1)));
     }
 
+    @Test
+    public void can_tile_be_put_state_not_PLACE_TILE() {
+        game.start(2);
+        assertThrows(IllegalStateException.class,
+                () -> game.canTileBePut(new Position(0, 0)));
+    }
+
     /* === Testing method isInside() === */
     @Test
     public void position_is_inside_true() {
@@ -139,11 +145,42 @@ public class GameTest {
         assertFalse(game.isInside(new Position(-1, 3)));
     }
 
+    @Test
+    public void position_is_inside_false_out_of_bound_1() {
+        game.start(2);
+        assertFalse(game.isInside(new Position(1000, 1000)));
+    }
+
+    @Test
+    public void position_is_inside_false_out_of_bound_2() {
+        game.start(2);
+        assertFalse(game.isInside(new Position(-5897, -625)));
+    }
+
+    @Test
+    public void position_is_inside_false_out_of_bound_3() {
+        game.start(2);
+        assertFalse(game.isInside(new Position(-50, 2)));
+    }
+
+    @Test
+    public void position_is_inside_false_out_of_bound_4() {
+        game.start(2);
+        assertFalse(game.isInside(new Position(1, -9)));
+    }
+
     /* === Testing method getWinner() === */
     @Test
     public void player_is_winner() {
         fullPlay();
         assertEquals(0, game.getWinner());
+    }
+
+    @Test
+    public void get_player_winner_state_not_GAME_OVER() {
+        game.start(2);
+        assertThrows(IllegalStateException.class,
+                () -> game.getWinner());
     }
 
     /* === Testing method getPickedTile() === */
@@ -152,6 +189,20 @@ public class GameTest {
         game.start(2);
         game.pickTile(4);
         assertEquals(4, game.getPickedTile().getValue());
+    }
+
+    @Test
+    public void pick_tile_5_true() {
+        Tile tile;
+        game.start(2);
+        tile = game.pickTile(5);
+        assertEquals(tile, game.getPickedTile());
+    }
+
+    @Test
+    public void pick_tile_state_not_PLACE_TILE() {
+        assertThrows(IllegalStateException.class,
+                () -> game.pickTile());
     }
 
     /* === Testing method putTile and getTile() === */
@@ -163,7 +214,68 @@ public class GameTest {
         assertEquals(2, game.getTile(0, new Position(0, 0)).getValue());
     }
 
-    /* === Testing method nextPlayer() === */
+    @Test
+    public void get_tile_state_NOT_STARTED() {
+        assertThrows(IllegalStateException.class,
+                () -> game.getTile(0, new Position(0, 0)));
+    }
+
+    @Test
+    public void put_tile_on_board_out_of_bound_negative_row() {
+        game.start(2);
+        game.pickTile();
+        assertThrows(IllegalArgumentException.class,
+                () -> game.putTile(new Position(-1, 0)));
+    }
+
+    @Test
+    public void put_tile_on_board_out_of_bound_negative_column() {
+        game.start(2);
+        game.pickTile();
+        assertThrows(IllegalArgumentException.class,
+                () -> game.putTile(new Position(0, -1)));
+    }
+
+    @Test
+    public void put_tile_on_board_out_of_bound_negative_both() {
+        game.start(2);
+        game.pickTile();
+        assertThrows(IllegalArgumentException.class,
+                () -> game.putTile(new Position(-1, -5)));
+    }
+
+    @Test
+    public void put_tile_on_board_out_of_bound_positive_row() {
+        game.start(2);
+        game.pickTile();
+        assertThrows(IllegalArgumentException.class,
+                () -> game.putTile(new Position(5, 0)));
+    }
+
+    @Test
+    public void put_tile_on_board_out_of_bound_positive_column() {
+        game.start(2);
+        game.pickTile();
+        assertThrows(IllegalArgumentException.class,
+                () -> game.putTile(new Position(0, 5)));
+    }
+
+    @Test
+    public void put_tile_on_board_out_of_bound_positive_both() {
+        game.start(2);
+        game.pickTile();
+        assertThrows(IllegalArgumentException.class,
+                () -> game.putTile(new Position(5, 5)));
+    }
+
+    @Test
+    public void put_tile_on_board_not_PLACE_TILE() {
+        game.start(2);
+        assertThrows(IllegalStateException.class,
+                () -> game.putTile(new Position(0, 0)));
+    }
+
+    /* === Testing method nextPlayer() and getCurrentPlayerNumber() === */
     @Test
     public void next_player() {
         game.start(2);
@@ -186,6 +298,19 @@ public class GameTest {
         // player 0
         game.nextPlayer();
         assertEquals(0, game.getCurrentPlayerNumber());
+    }
+
+    @Test
+    public void get_current_player_number_state_NOT_STARTED_GAME_OVER() {
+        assertThrows(IllegalStateException.class,
+                () -> game.getCurrentPlayerNumber());
+    }
+
+    @Test
+    public void next_player_state_not_TURN_END() {
+        game.start(2);
+        assertThrows(IllegalStateException.class,
+                () -> game.nextPlayer());
     }
 
     /* === Testing method getState() === */
@@ -215,5 +340,18 @@ public class GameTest {
         fullPlay();
         assertEquals(State.GAME_OVER, game.getState());
     }
-    
+
+    /* === Testing method getPlayerCount() === */
+    @Test
+    public void get_player_count_state_not_NOT_STARTED() {
+        assertThrows(IllegalStateException.class,
+                () -> game.getPlayerCount());
+    }
+
+    @Test
+    public void get_player_count_2_players() {
+        game.start(2);
+        assertEquals(2, game.getPlayerCount());
+    }
+
 }
