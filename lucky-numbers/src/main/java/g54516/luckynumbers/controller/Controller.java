8fa87ed829906452c4255d0bcf0bc71af767198b
54516 <54516@etu.he2b.be>
2021-04-28 10:06:45 +0200

    Modifications de la classe Controller

--- lucky-numbers/src/main/java/g54516/luckynumbers/controller/Controller.java
+++ lucky-numbers/src/main/java/g54516/luckynumbers/controller/Controller.java
@@ -34,7 +34,6 @@ public class Controller {
         // initialize variables
         Scanner kbd = new Scanner(System.in);
         String endGame = "";
-        Position pos;
 
         // display welcome message
         view.displayWelcome();
@@ -52,73 +51,18 @@ public class Controller {
 
                 // If current state is 'PICK_TILE'
                 case PICK_TILE:
-                    // Displays the deck
-                    view.displayDeck();
-                    // If there is face up tiles
-                    if (game.faceUpTileCount() != 0) {
-                        // Asks player which action he want do :
-                        // Pick face down or pick face up
-                        String choice = view.askAction();
-                        // Displays the deck
-                        view.displayDeck();
-                        // If player answer 'u' it mean that he want
-                        // pick face up tile
-                        if (choice.contains("u")) {
-                            game.pickFaceUpTile(new Tile(view.askWhichFaceUpTile()));
-                            // Else he pick face down tile
-                        } else {
-                            game.pickFaceDownTile();
-                        }
-                        // Else there is not face up tile the player pick
-                        // automatically face down tile
-                    } else {
-                        game.pickFaceDownTile();
-                    }
+                    this.pickUpOrDown();
                     break;
 
                 // If the state is 'PLACE_OR_DROP_TILE'
                 case PLACE_OR_DROP_TILE:
-                    // Displays the board
-                    view.displayGame();
-                    // Asks player if he want put his tile or drop it
-                    String dropOrPut = view.askDropOrPut();
-                    // if the player answer 'd' it mean that he want drop 
-                    // the tile
-                    if (dropOrPut.contains("d")) {
-                        game.dropTile();
-                        // Else he must put the tile on his board
-                    } else {
-                        // Asks player in which position he want to put his
-                        // tile
-                        pos = view.askPosition();
-                        // If the given position by the player is wrong
-                        // (don't respect rules or outside the board)
-                        // display an error message
-                        if (!game.canTileBePut(pos)) {
-                            view.displayError("Cannot put a tile on that position\n");
-                            // Else put the tile on the given position
-                        } else {
-                            game.putTile(pos);
-                        }
-                    }
+                    this.dropOrPut();
                     break;
 
                 // If the current state is 'PLACE_TILE'
                 // (if the player picks face up tile)
                 case PLACE_TILE:
-                    // Displays the board
-                    view.displayGame();
-                    // Asks player in which position he want put his tile
-                    pos = view.askPosition();
-                    // If the given position by the player is wrong
-                    // (don't respect rules or outside the board)
-                    // display an error message
-                    if (!game.canTileBePut(pos)) {
-                        view.displayError("Cannot put a tile on that position\n");
-                    } else {
-                        // Else put the tile on the given position
-                        game.putTile(pos);
-                    }
+                    this.putTile();
                     break;
 
                 // If the current state is 'TURN_END'
@@ -148,6 +92,90 @@ public class Controller {
                 // the 'break' is to stop/break/exit the while condition
                 break;
             }
+        }
+    }
+
+    /**
+     * Asks player if he want to pick face up or face down tile.
+     */
+    private void pickUpOrDown() {
+        Scanner kbd = new Scanner(System.in);
+
+        // Displays the deck
+        view.displayDeck();
+        // If there is face up tiles
+        if (game.faceUpTileCount() != 0) {
+            // Asks player which action he want do :
+            // Pick face down or pick face up
+            String choice = view.askAction();
+            // Displays the deck
+            view.displayDeck();
+            // If player answer 'u' it mean that he want
+            // pick face up tile
+            if (choice.contains("u")) {
+                game.pickFaceUpTile(new Tile(view.askWhichFaceUpTile()));
+                // Else he pick face down tile
+            } else {
+                game.pickFaceDownTile();
+            }
+            // Else there is not face up tile the player pick
+            // automatically face down tile
+        } else {
+            game.pickFaceDownTile();
+        }
+    }
+
+    /**
+     * Asks player if he want to drop or put the tile.
+     */
+    private void dropOrPut() {
+        Scanner kbd = new Scanner(System.in);
+        Position pos;
+
+        // Displays the board
+        view.displayGame();
+        // Asks player if he want put his tile or drop it
+        String dropOrPut = view.askDropOrPut();
+        // if the player answer 'd' it mean that he want drop 
+        // the tile
+        if (dropOrPut.contains("d")) {
+            game.dropTile();
+            // Else he must put the tile on his board
+        } else {
+            // Asks player in which position he want to put his
+            // tile
+            pos = view.askPosition();
+            // If the given position by the player is wrong
+            // (don't respect rules or outside the board)
+            // display an error message
+            if (!game.canTileBePut(pos)) {
+                view.displayError("Cannot put a tile on that position\n");
+                // Else put the tile on the given position
+            } else {
+                game.putTile(pos);
+            }
+        }
+    }
+
+    /**
+     * Asks player the position where he want to put a tile.
+     */
+    private void putTile() {
+        Scanner kbd = new Scanner(System.in);
+        Position pos;
+
+        // Displays the board
+        view.displayGame();
+        // Asks player in which position he want put his tile
+        pos = view.askPosition();
+        // If the given position by the player is wrong
+        // (don't respect rules or outside the board)
+        // display an error message
+        if (!game.canTileBePut(pos)) {
+            view.displayError("Cannot put a tile on that position\n");
+        } else {
+            // Else put the tile on the given position
+            game.putTile(pos);
         }
     }
 
